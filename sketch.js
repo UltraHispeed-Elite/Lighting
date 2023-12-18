57266d0595d8d5c68e6ccf9404b43a68cbf3161f
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2023-12-18 14:16:37 -0600

    version 2
    much more customizable.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,5 @@
 var player, playerImg;
-var a = [0,0];
-var b = [0,0];
-var c = [0,0];
-var d = 0;
+var c;
 
 function preload() {
   playerImg = loadImage("player.png");
@@ -12,58 +9,71 @@ function setup() {
   createCanvas(400,400);
   
   player = createSprite(200,200,50,50);
-  player.addImage(playerImg);
+  player.addImage("player", playerImg);
+  tint(255, 0, 0, 100);
+  
+  createLight(0,400,0,400);
 }
 
 function draw() {
   background("black");
   
-  tint(255,255,255,d);
-  
-   if(player.x < 200) {
-     b[0] = 0;
-     a[0] = 2.55 * (b[0]+(Math.abs(player.x)/2))
-   }else if(player.x > 200) {
-     b[0] = 100;
-     a[0] = 2.55 * ((b[0]+100)-(Math.abs(player.x)/2))
-   }else {
-     b[0] = 100;
-     a[0] = 2.55*b[0];
-   }
-   c[0] = Math.abs(a[0]);
-  
-  if(player.y < 200) {
-    b[1] = 0;
-    a[1] = 2.55 * (b[1]+(Math.abs(player.y)/2))
-  }else if(player.y > 200) {
-    b[1] = 100;
-    a[1] = 2.55 * ((b[1]+100)-(Math.abs(player.y)/2))
-  }else {
-    b[1] = 100;
-    a[1] = 2.55*b[1];
-  }
-  c[1] = Math.abs(a[1]);
-  
-  
-  d = (c[0]+c[1])/2;
-  
-
-  
-  playerMovement()
+  playerMovement();
+  playerLight();
   
   drawSprites();
 }
 
+function createLight(x1, x2, y1, y2) {
+  let xd = x2-x1;
+  let yd = y2-y1;
+  let cx = xd/2;
+  let cy = yd/2;
+  c = (cx+cy)/2;
+}
+
+function playerLight() {
+   let d,e,g,h,vx,tv;
+  if(player.x < c) {
+    d = c-player.x;
+    e = (d/c)*100
+  }else if(player.x > c) {
+    d = player.x-c;
+    e = (d/c)*100
+  }else {
+    d = 0;
+    e = 0;
+  }
+
+  if(player.y < c) {
+    g = c-player.y;
+    h = (g/c)*100
+  }else if(player.y > c) {
+    g = player.y-c;
+    h = (g/c)*100
+  }else {
+    g = 0;
+    h = 0;
+  }
+
+  vx = 100 - e;
+  vy = 100 - h;
+  ptv = (vx+vy)/2;
+  tv = 2.55*ptv;
+  tint(255,255,255,tv);
+  console.log(tv);
+}
+
 function playerMovement() {
-  if(keyDown("w")) {
+  if(keyDown('w')) {
     player.y -= 5;
   }
-  
+
   if(keyDown("a")) {
     player.x -= 5;
   }
-  
-  if(keyDown("s")) {
+
+  if(keyDown('s')) {
     player.y += 5;
   }
   
